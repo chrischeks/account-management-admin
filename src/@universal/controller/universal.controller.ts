ab80df5c386511c8b81c8abf0adbfd735a76a8a7
Chekwube <chekwube@rubikpay.net>
2021-08-09 21:52:01 +0100

    fixed issue with heroku deployment

--- src/@universal/controller/universal.controller.ts
+++ src/@universal/controller/universal.controller.ts
@@ -1,18 +1,18 @@
 import IResponse from '@/@universal/interfaces/response.interface';
-import { Response } from 'express';
-import { RequestWithCustomer } from '../interfaces/request.interface';
+import { Response, Request } from 'express';
+import { ICustomer } from '../interfaces/customer.interface';
+// import { RequestWithCustomer } from '../interfaces/request.interface';
 import { logger } from '../logger/logger';
 
 class UniversalController {
-  protected controllerErrorHandler = async (req: RequestWithCustomer, res: Response, error) => {
+  protected controllerErrorHandler = async (req: Request & { customer: ICustomer }, res: Response, error) => {
     const { originalUrl, method, ip, customer, body } = req;
-    console.log(error, 'errorrrrrrrrrrrrrrrrrrrrrrrr');
 
     logger.log('error', `URL:${originalUrl} - METHOD:${method} - IP:${ip} - ERROR:${error}- Identifier:${customer?.email || body?.email}`);
     return res.status(500).json({ status: false, message: 'Operation was not successful, please contact support.', data: null });
   };
 
-  public controllerResponseHandler = async (response: IResponse, req: RequestWithCustomer, res: Response) => {
+  public controllerResponseHandler = async (response: IResponse, req: Request & { customer: ICustomer }, res: Response) => {
     const { statusCode, status, message, data } = response;
     const { originalUrl, method, ip, body, customer } = req;
     logger.log(
